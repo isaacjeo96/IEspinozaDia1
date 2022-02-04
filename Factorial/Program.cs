60f689ce692f8659f94663785c4eb1cf4abcccab
isaacjeo96 <isaacjeo96@outlook.com>
2022-02-03 23:16:07 -0600

    Agregar archivos de proyecto.

---
+++ Factorial/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        public static Int32 Factorial(int Numero)
+        {
+
+            if (Numero == 1)
+            {
+                return 1;
+               
+            }
+
+            return Numero * Factorial(Numero - 1);           
+
+        }
+        static void Main(string[] args)
+        {
+            int Numero;
+            Console.WriteLine("Introduzca un numero: ");
+            Numero = Convert.ToInt32(System.Console.ReadLine());
+            Console.WriteLine("El factorial es: " + Factorial(Numero));
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Operadores/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores
+{
+    class Program
+    {
+        public int Suma(int Numero1, int Numero2)
+        {
+            int Resultado = Numero1 + Numero2;
+            return Resultado;
+        }
+
+        public int Resta(int Numero1, int Numero2)
+        {
+           int Resultado = Numero1 - Numero2;
+            return Resultado;
+        }
+
+        public int Multiplicacion(int Numero1, int Numero2)
+        {
+           int Resultado = Numero1 * Numero2;
+            return Resultado;
+        }
+
+        public int Division(int Numero1, int Numero2)
+        {
+           int Resultado = Numero1 / Numero2;
+            return Resultado;
+        }
+        static void Main(string[] args)
+        {
+            Program operacion = new Program();
+
+            Console.WriteLine("El resultado de la suma es:" + operacion.Suma(5,8));
+            Console.WriteLine("El resultado de la resta es:" + operacion.Resta(10,5));
+            Console.WriteLine("El resultado de la multiplicacion es:" + operacion.Multiplicacion(12,2));
+            Console.WriteLine("El resultado de la division es:" + operacion.Division(100,2));
+            
+            
+            Console.ReadLine(); 
+        }
+    }
+}
